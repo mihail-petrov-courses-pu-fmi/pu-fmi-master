36e8d796735c1de7ea4d759127c32342232a5416
Mihail Petrov <mail@mihail-petrov.me>
2024-11-03 12:12:46 +0200

    update v1 meet_04

---
+++ 24-25/meet-04/source/v1/solarparkapi/src/main/java/com/project02_solar_park_api/solarparkapi/SolarparkapiApplication.java
@@ -0,0 +1,13 @@
+package com.project02_solar_park_api.solarparkapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolarparkapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolarparkapiApplication.class, args);
+	}
+
+}
